6f7d2693c323acfeea5ca43ed4ef41420cc491d1
Anton Kuzmenko <ayukuzmenko@gmail.com>
2019-02-04 00:21:38 +0300

    Add search bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Map from './map';
+import SearchBar from './search-bar'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Map ymaps={this.props.ymaps}/>
+        <SearchBar ymaps={this.props.ymaps}/>
       </div>
     );
   }
---
+++ src/components/search-bar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  state = {
+    map: null
+  }
+
+  initSearch = () => {
+    const {ymaps} = this.props;
+    var suggestView1 = new ymaps.SuggestView('suggest1');
+    console.log(suggestView1);
+  }
+
+  render() {
+    return(
+      <div>
+        <p class="header">Начните вводить запрос для появления поисковой подсказки</p>
+        <input type="text" id="suggest1"/>
+      </div>
+      );
+  }
+
+  componentDidMount() {
+    const {ymaps} = this.props;
+    ymaps.ready(this.initSearch);
+  }
+}
+
+export default SearchBar;
