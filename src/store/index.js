c46c9633de5c3f7d5421b11a93e24957bd15953a
Anton Kuzmenko <ayukuzmenko@gmail.com>
2019-03-23 19:10:16 +0300

    Refact

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,6 @@
-// import { applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
 import { init } from '@rematch/core';
 import immerPlugin from '@rematch/immer';
 import { points } from '../models/points';
-
-// let composeEnhancers;
-// if (process.env.NODE_ENV === 'development') {
-//   composeEnhancers =
-//     typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//       ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-//       : compose;
-// } else {
-//   composeEnhancers = compose;
-// }
-
-// const enhancer = composeEnhancers(applyMiddleware(thunk, randomid));
-
-//const store = createStore(reducer, enhancer);
 
 const immer = immerPlugin();
 
