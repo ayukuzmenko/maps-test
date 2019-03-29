e5052141296bfced28c52535ac23ff5e573b85bf
Anton Kuzmenko <ayukuzmenko@gmail.com>
2019-03-30 00:30:42 +0300

    Add delete point test

--- src/components/point/point.spec.js
+++ src/components/point/point.spec.js
@@ -6,12 +6,37 @@ describe('Point', () => {
   it('Add new point', () => {
     const item = { adress: 'Moscow' };
     const pointContainer = shallow(<Point item={item} />);
+
     expect(pointContainer).toMatchSnapshot();
+
     expect(
       pointContainer
         .find('span')
         .first()
         .text(),
     ).toEqual('Moscow');
+
+    expect(
+      pointContainer
+        .find('button')
+        .first()
+        .text(),
+    ).toEqual('x');
+  });
+
+  it('Delete a point from list', () => {
+    const mockDeletePoint = jest.fn();
+    const props = {
+      item: { adress: 'Moscow' },
+      deletePoint: mockDeletePoint,
+    };
+
+    const pointContainer = shallow(<Point {...props} />);
+    pointContainer
+      .find('button')
+      .first()
+      .simulate('click');
+
+    expect(mockDeletePoint).toHaveBeenCalledTimes(1);
   });
 });
