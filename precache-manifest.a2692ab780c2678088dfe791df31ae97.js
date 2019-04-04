c767bda877ca78a895d17b3b69d73cdb11bf3169
Anton Kuzmenko <ayukuzmenko@gmail.com>
2019-04-04 15:14:13 +0300

    Updates

---
+++ precache-manifest.a2692ab780c2678088dfe791df31ae97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fb35af1798109a8ddf2",
+    "url": "/maps-test/static/js/runtime~main.5fb35af1.js"
+  },
+  {
+    "revision": "14cb52c4167a54ead10c",
+    "url": "/maps-test/static/js/main.14cb52c4.chunk.js"
+  },
+  {
+    "revision": "804268b79b7772e19acc",
+    "url": "/maps-test/static/js/1.804268b7.chunk.js"
+  },
+  {
+    "revision": "14cb52c4167a54ead10c",
+    "url": "/maps-test/static/css/main.27dadae1.chunk.css"
+  },
+  {
+    "revision": "16a860ac2be024514e5c7c453b66058e",
+    "url": "/maps-test/index.html"
+  }
+];
